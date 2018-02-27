75052e408652c4179f3edaba0fd1e000d157a6b6
Joseph <josephbarban@gmail.com>
2018-02-27 20:57:42 +0100

    ok with sub-domains

--- ecochain-blog-fe/src/AppConfig.ts
+++ ecochain-blog-fe/src/AppConfig.ts
@@ -1,6 +1,6 @@
 class AppConfig {
   devMode: boolean = false;
-  baseUrl: string = 'http://localhost'; // 'http://api.blog.ecochain.com';
+  baseUrl: string = 'http://blogapi.vps514678.ovh.net.com:81';
 
   numberOfArticlesPerPage: number = 2;
 }
